cded7362d18d701283ecf78da4d0241019265a3d
pothur909 <chowdaryp697@gmail.com>
2025-12-25 13:07:09 +0530

    cors

--- backend/app.js
+++ backend/app.js
@@ -26,6 +26,7 @@ app.use(cors({
     'http://localhost:3000', 
     'http://127.0.0.1:3000',
     'https://we-vd27.vercel.app',
+    'https://www.weforuni.com',
     /\.vercel\.app$/  // Allow all Vercel preview deployments
   ],
   credentials: true
